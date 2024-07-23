bacb72c305c0683a47509163f1ebb6ea03e14660
Charles19901210 <charles@aruvr.com>
2024-07-23 01:29:41 +0100

    test a-text

--- src/views/LocationAR.vue
+++ src/views/LocationAR.vue
@@ -8,14 +8,20 @@
       embedded
       arjs="sourceType: webcam; locationOnly: true; debugUIEnabled: false;"
     >
-      <a-entity
+      <!-- <a-entity
         id="model"
         :position="modelPosition"
         gltf-model="./assets/map-pin.glb"
         scale="2 2 2"
-        visible="true"
-      ></a-entity>
+        :visible="isWithinRange"
+      ></a-entity> -->
       <!-- :visible="isWithinRange" -->
+      <a-text
+        value="This content will always face you."
+        look-at="[gps-camera]"
+        scale="50 50 50"
+        :gps-entity-place="`latitude: ${latitude}; longitude: ${longitude};`"
+      ></a-text>
       <a-camera id="camera" gps-camera rotation-reader></a-camera>
     </a-scene>
   </div>
